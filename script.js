0c59bff5d949d65ff4e09efd1e3e788ab5685c9f
tylightning <Tirike.nyc@gmail.com>
2017-03-22 22:00:19 +0000

    update with json stater

--- script.js
+++ script.js
@@ -15,7 +15,10 @@
                         var zipcode = $("Zip").val();
 
                             var url = "https://api.foursquare.com/v2/venues/search?near=" +
-                                zipcode + "&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZ"
+                                zipcode + "&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZS"
 
 
                                             }
+
+// json 
+// https://api.foursquare.com/v2/venues/search?near=60646&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZS&v=20170122
