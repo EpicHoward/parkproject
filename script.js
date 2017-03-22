b1119bdb3ab001b6a5651770b6a422fbb5ce57b2
tylightning <Tirike.nyc@gmail.com>
2017-03-22 21:51:28 +0000

    update

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-$(document).ready(function()
-
-        $("#button").on("click", function() {
+    // CLIENT_ID : GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA
 
 
-                $("#zip").val();
-
-                var zipcode = $("Zip").val();
-
-                var url = "https://api.foursquare.com/v2/venues/search?near=" +
-                zipcode + "&query= =21b0a78e60bc539c02c647c6f01ebfba"
-                
-
-            }
+      // CLIENT_SECRET : HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZ
 
 
-            // CLIENT_ID : GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA
+
+        $(document).ready(function()
+
+              $("#button").on("click", function() {
 
 
-            // CLIENT_SECRET : HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZ
+                     $("#zip").val();
 
+                        var zipcode = $("Zip").val();
+
+                            var url = "https://api.foursquare.com/v2/venues/search?near=" +
+                                zipcode + "&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZ"
+
+
+                                            }
