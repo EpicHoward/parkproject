6db7af6ac7e8fce74c29cd1204feda6845eeda95
tylightning <Tirike.nyc@gmail.com>
2017-03-22 20:59:33 +0000

    updatee

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-document.ready() 
+$(document).ready(function()
+
+        $("#button").on("click", function() {
 
 
-// CLIENT_ID : GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA
+                $("#zip").val();
+
+                var zipcode = $("Zip").val();
+                
+                var url = "https://api.foursquare.com/v2/venues/search"
+
+            }
 
 
-// CLIENT_SECRET : HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZ
+            // CLIENT_ID : GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA
+
+
+            // CLIENT_SECRET : HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZ
