ef64fb3d3d99da661344615078c594a74f0d1bf6
tylightning <Tirike.nyc@gmail.com>
2017-03-29 22:08:49 +0000

    update

--- script.js
+++ script.js
@@ -16,9 +16,19 @@
 
                             var url = "https://api.foursquare.com/v2/venues/search?near=" +
                                 zipcode + "&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZS"
-
-
+                                    var parkapiId = "4bf58dd8d48988d163941735"{
+                                       
+                                     // For (x=0, x<venues.lenght, x++);
+                                        if x= parkapiId
+                                        then 
+                                        return(name+adress+crossStreet);
+                                    
+                                    }
                                             }
+                                            First run code to look into  the reponse then
+                                            venues then
+                                            look for catagorie parkapiId
+                                            then return name, adress, crossStreet
 
 // json 
 // https://api.foursquare.com/v2/venues/search?near=60646&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZS&v=20170122
