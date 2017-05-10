d87ada41c5f2b0a9ec9cdfa6bc6c50edd8dd52fc
tylightning <Tirike.nyc@gmail.com>
2017-05-10 22:06:00 +0000

    started writing the google maps api program , need to append address of park into google api

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
     // CLIENT_SECRET : HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZ
 
- //https://api.foursquare.com/v2/venues/search?near=60646&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZS&v=20170122
-    
+    //https://api.foursquare.com/v2/venues/search?near=60646&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZS&v=20170122
+
     //google maps api key : AIzaSyC6rKKqP2muUpfaai3uth3ro_e97jcUYiQ
 
     $(document).ready(function() {
@@ -15,25 +15,30 @@
             var url = "https://api.foursquare.com/v2/venues/search?near=" +
                 zipcode + "&query=park&client_id=GHRILCJUTG0XCRR1SB3SXD52IPBKCB1PWLUNVSW45KQ5S4HA&client_secret=HD35RTRLE24TWUHLFDWM3OJNT2ILW44CA1TRO20AJRZ5JSZS" +
                 "&v=20170122&m=foursquare";
+                
+                
+                      
+            var place ="https://www.google.com/maps/embed/v1/place?" + "key=AIzaSyC6rKKqP2muUpfaai3uth3ro_e97jcUYiQ" +
+            
+            // add append parameters here and get it inside the i-frame
 
 
-            var parkapiId = "4bf58dd8d48988d163941735"
-
+    
             console.log(url);
 
             $.get(url, function(data) {
 
-                $("#address").append(data.response.venues[0].location.address);
+                $("#address").empty().append(data.response.venues[0].location.address);
 
-                $("#city").append(data.response.venues[0].location.city);
+                $("#city").empty().append(data.response.venues[0].location.city);
 
-                $("#country").append(data.response.venues[0].location.country);
+                $("#country").empty().append(data.response.venues[0].location.country);
 
-                $("#state").append(data.response.venues[0].location.state);
+                $("#state").empty().append(data.response.venues[0].location.state);
 
-                $("#crosstreet").append(data.response.venues[0].location.crossStreet);
+                $("#crosstreet").empty().append(data.response.venues[0].location.crossStreet);
 
-                $("#name").append(data.response.venues[0].name);
+                $("#name").empty().append(data.response.venues[0].name);
 
             });
 
@@ -44,16 +49,4 @@
 
 
     });
-
-
-
-
-
-
-
-
-
-
-
-
- 
+    
